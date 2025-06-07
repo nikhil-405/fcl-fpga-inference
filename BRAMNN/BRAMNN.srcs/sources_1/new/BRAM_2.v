0bede674aaa64e8778d068ad5da6e7597d9b15bc
Nikhil Goyal <148058602+nikhil-405@users.noreply.github.com>
2025-06-07 20:57:03 +0530

    Delete BRAMNN directory

--- BRAMNN/BRAMNN.srcs/sources_1/new/BRAM_2.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module BRAM_2(clk, en, ren, wen, addr, din, dout);
-input clk, en, wen, ren;
-input [3:0] addr;
-input [7:0] din;
-output [7:0] dout;
-blk_mem_gen_1 inst2(.clka(clk),.ena((en&ren)|(en&wen)),.wea(wen),.addra(addr),.dina(din),.douta(dout));
-endmodule
--- BRAMNN/BRAMNN.srcs/sources_1/new/BRAM_W_1.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module BRAM_W_1(clk, en, ren, wen, addr, din, dout);
-input clk, en, wen, ren;
-input [17:0] addr;
-input [7:0] din;
-output [7:0] dout;
-FCLW1 inst3(.clka(clk),.ena((en&ren)|(en&wen)),.wea(wen),.addra(addr),.dina(din),.douta(dout));
-endmodule
